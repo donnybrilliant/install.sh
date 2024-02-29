51ee2b9c1e7a68018a1e3007ccca9b8fccd9c0cf
donnybrilliant <37931601+donnybrilliant@users.noreply.github.com>
2024-02-29 14:39:10 +0100

    Adds VSCode extensions and fixes bugs

--- README.md
+++ README.md
@@ -6,15 +6,12 @@ mbp 14 2021
 
 Fresh Install Script for macOS
 
-Personal script for essential apps and configuration after a fresh install of macOS.
+Script for installing essential apps and configuration after a fresh install of macOS.
 
 To do:
 
-- Other deployment CLIs?
-- Install Unity or Hub?
-- Database packages
-- Create brewfile instead.
-- MAS.
+- Create universal config file
+- Create brewfile instead?
 
 Will be adding configuration and package changes for personal taste.
 
@@ -22,8 +19,5 @@ Feature ideas:
 
 - Checklist
 - ASCII Graphics over install
-- VSCode plugins & settings
-- Finder list view
+- VSCode, Transmission settings etc
 - Log out of FaceTime/Messages
-- Auto updates
-- Transmission (etc) settings
--- install.sh
+++ install.sh
@@ -2,55 +2,53 @@
 
 # ESSENTIAL PACKAGES
 
+CASKS=(
+  iterm2
+  google-chrome
+  arc
+  visual-studio-code
+  spotify
+  vlc
+  dropbox
+  google-drive
+  sync
+  github
+  cyberduck
+  postman
+  discord
+  microsoft-teams
+  silicon
+  qflipper
+  pieces
+  ollama
+  devtoys
+  the-unarchiver
+  cheatsheet
+  zenmap
+  tiled
+  arduino-ide
 
-    ESSENTIALS=(
-        google-chrome
-        iterm2
-        zsh
-        speedtest-cli
-        tree
-        dropbox
-        google-drive
-        sync
-        nmap
-        zenmap
-        spotify
-        vlc
-        visual-studio-code
-        wget
-        the-unarchiver
-        cheatsheet
-        git
-        github
-        postman
-        discord
-        silicon
-        cyberduck
-        microsoft-teams
-        qflipper
-        devtoys
-        tiled
-        arc
-        pieces
-        ollama
-        arduino-ide
 )
 
-# NPM PACKAGES 
+FORMULAE=(
+  speedtest-cli
+  tree
+  nmap
+  wget
+  git
+  pinentry-mac
+)
 
+# NPM PACKAGES
 
-      NPMPACKAGES=(
-        express
-      )
-
-
+NPMPACKAGES=(
+  express
+)
 
 # COLOR
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-
-
 
 ###########################################
 # Start
@@ -70,198 +68,207 @@ echo Enter root password
 sudo -v
 
 # Keep Sudo Until Script is finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
 
 # Update macOS // is sudo needed?
+echo
 echo "${GREEN}Looking for updates.."
 echo
 sudo softwareupdate -i -a
-clear
 
 # Homebrew
+echo
 echo "${GREEN}Installing Homebrew"
 echo
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-clear
+
+# Append Homebrew initialization to .zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>${HOME}/.zprofile
+# Immediately evaluate the Homebrew environment settings for the current session
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Update, upgrade and clean
+echo
 echo "${GREEN}Cleaning up.."
 echo
 brew update && brew upgrade && brew cleanup && brew doctor
-clear
-
 
 # Install essentials
-echo "${GREEN}Installing essentials..."
 echo
-brew install ${ESSENTIALS[@]}
-clear
+echo "${GREEN}Installing formulae..."
+brew install ${FORMULAE[@]}
+echo
+echo "${GREEN}Installing casks..."
+brew install --cask ${CASKS[@]}
 
 # Install Node
-echo -n "${RED}Install Node via NVM or Brew? ${NC}[N/b]"
-    read REPLY
-    echo    
-    if [[ $REPLY =~ ^[Nn]$ ]]
-    then
-      echo "${GREEN}Installing NVM..."
-      echo
-         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-
-
-    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-    clear
-
-      echo "${GREEN}Installing Node via NVM..."
-      echo
-      nvm install node
-      nvm use node
-      clear
-    fi
-    if [[ $REPLY =~ ^[Bb]$ ]]
-    then
-      echo "${GREEN}Installing Node via Homebrew..."
-      echo
-      brew install node
-      clear
-
-    fi
-
-echo "${GREEN}Installing Global NPM Packages..."
 echo
-npm install -g ${NPMPACKAGES[@]}
-clear
+echo -n "${RED}Install Node via NVM or Brew? ${NC}[N/b]"
+read REPLY
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+  echo "${GREEN}Installing NVM..."
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
+  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+  echo "${GREEN}Installing Node via NVM..."
+  nvm install --lts
+  nvm install node
+  nvm alias default node
+  nvm use default
+
+fi
+if [[ $REPLY =~ ^[Bb]$ ]]; then
+  echo "${GREEN}Installing Node via Homebrew..."
+  brew install node
+
+fi
+
+echo
+echo "${GREEN}Installing Global NPM Packages..."
+npm install -g ${NPMPACKAGES[@]}
 
 # Optional Packages
-
+echo
 echo -n "${RED}Install .NET? ${NC}[y/N]"
 read REPLY
-echo   
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   brew install dotnet
-    fi
-clear
+  export DOTNET_ROOT="/opt/homebrew/opt/dotnet/libexec"
+fi
 
+echo
 echo -n "${RED}Install Firefox Developer? ${NC}[y/N]"
 read REPLY
-echo   
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  brew tap homebrew/cask-versions
   brew install firefox-developer-edition
-    fi
-clear
+fi
 
+echo
 echo -n "${RED}Install Figma? ${NC}[y/N]"
 read REPLY
-echo   
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   brew install figma
-    fi
-clear
+fi
 
+echo
 echo -n "${RED}Install Netlify CLI? ${NC}[y/N]"
 read REPLY
-echo   
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   brew install netlify-cli
-    fi
-clear
+fi
 
+echo
 echo -n "${RED}Install Databases? ${NC}[y/N]"
 read REPLY
-echo   
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-          brew install postgresql
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  brew install postgresql
 
-          brew install mysql 
-          brew services restart mysql
-          mysql_secure_installation
+  brew install mysql
+  brew services restart mysql
+  mysql_secure_installation
 
-        brew tap mongodb/brew
-        brew install mongodb-community
-    fi
-clear
+  brew tap mongodb/brew
+  brew install mongodb-community
+fi
 
+echo
 echo -n "${RED}Install Epic & Steam ${NC}[y/N]"
 read REPLY
-echo   
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   brew install steam epic-games
-    fi
-clear
+fi
 
+echo
 echo -n "${RED}Install Unity Hub? ${NC}[y/N]"
 read REPLY
-echo   
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   brew install unity-hub
-    fi
-clear
+fi
 
+echo
 echo -n "${RED}Install Binance? ${NC}[y/N]"
 read REPLY
-echo   
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   brew install binance
-    fi
-clear
+fi
 
-
+echo
 echo -n "${RED}Install Transmission? ${NC}[y/N]"
 read REPLY
-echo   
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   brew install transmission
   brew install --cask transmission
-    fi
-clear
+fi
 
+echo
 echo -n "${RED}Install Balena Etcher? ${NC}[y/N]"
 read REPLY
-echo   
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   brew install balenaetcher
-    fi
-clear
+fi
 
 # Cleanup
-echo "${GREEN}Cleaning up..."
 echo
+echo "${GREEN}Cleaning up..."
 brew update && brew upgrade && brew cleanup && brew doctor
+mkdir -p /Users/daniel/Library/LaunchAgents
 brew tap homebrew/autoupdate
 brew autoupdate start 86400 --upgrade --cleanup --immediate --sudo
-clear
 
 # Settings
-echo "${GREEN}Configuring default settings..."
 echo
-defaults write -g com.apple.mouse.scaling 3
-defaults write -g com.apple.trackpad.scaling 3
-defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode TwoButton
-defaults write com.apple.AppleMultitouchMouse.plist MouseButtonMode TwoButton
-defaults write -g AppleShowAllExtensions -bool true
-defaults write com.apple.finder AppleShowAllFiles true
-defaults write com.apple.finder ShowPathbar -bool true
-defaults write com.apple.finder ShowStatusBar -bool true
-defaults write com.apple.finder NewWindowTarget PfHm
-chflags nohidden ~/Library
-clear
+echo -n "${RED}Configure default settings? ${NC}[y/N]"
+read REPLY
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "${GREEN}Configuring default settings..."
+  defaults write -g com.apple.mouse.scaling 3
+  defaults write -g com.apple.trackpad.scaling 3
+  defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode TwoButton
+  defaults write com.apple.AppleMultitouchMouse.plist MouseButtonMode TwoButton
+  defaults write -g AppleShowAllExtensions -bool true
+  defaults write com.apple.finder AppleShowAllFiles true
+  defaults write com.apple.finder ShowPathbar -bool true
+  defaults write com.apple.finder ShowStatusBar -bool true
+  defaults write com.apple.finder NewWindowTarget PfHm
+  defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+  defaults write com.apple.finder _FXSortFoldersFirst -bool true
+  chflags nohidden ~/Library
+fi
+
+# Dock settings
+## Very little flexible.. Based on language.. And all fails if one fails..
+echo
+echo -n "${RED}Apply Dock settings?? ${NC}[y/N]"
+read REPLY
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  dockutil --add /Applications/Google\ Chrome.app --replacing 'Safari' &&
+    dockutil --add /Applications/iTerm.app &&
+    dockutil --add /Applications/Visual\ Studio\ Code.app &&
+    dockutil --add /Applications/GitHub\ Desktop.app &&
+    dockutil --add /Applications/Spotify.app &&
+    dockutil --add /Applications/Discord.app &&
+    dockutil --add /Applications/Microsoft\ Teams\ \(work\ or\ school\).app &&
+    dockutil --remove 'Meldinger' &&
+    dockutil --remove 'Kart' &&
+    dockutil --remove 'Bilder' &&
+    dockutil --remove 'Påminnelser' &&
+    dockutil --remove 'FaceTime' &&
+    dockutil --remove 'Kontakter' &&
+    dockutil --remove 'TV' &&
+    dockutil --remove 'Musikk' &&
+    dockutil --remove 'Mail'
+fi
 
 # Git Login
-
-echo "${GREEN}GIT LOGIN"
 echo
+echo "${GREEN}GIT LOGIN"
 echo
 
 echo "${RED}Please enter your git username:${NC}"
@@ -273,27 +280,59 @@ git config --global user.name "$name"
 git config --global user.email "$email"
 git config --global color.ui true
 
-printf "${GREEN}GITTY UP!"
-clear
+echo
+echo "${GREEN}GITTY UP!"
+
+# VS Code Extensions
+echo
+echo -n "${RED}Install VSCode Extensions? ${NC}[y/N]"
+read REPLY
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  code --install-extension esbenp.prettier-vscode
+  code --install-extension GitHub.copilot
+  code --install-extension dsznajder.es7-react-js-snippets
+  code --install-extension ritwickdey.liveserver
+  code --install-extension github.vscode-pull-request-github
+  code --install-extension sourcegraph.cody-ai
+  code --install-extension eamodio.gitlens
+  code --install-extension meshintelligenttechnologiesinc.pieces-vscode
+#auto rename
+#auto closE?
+#color brackets
+#html?
+#typescript?
+#typescript react?
+#other intellisenses?
+fi
+
+# MAS
+echo
+echo -n "${RED}Do you want to install Pages, Numbers & Trello from App Store? ${NC}[y/N]"
+read REPLY
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  brew install mas
+  mas install 409201541
+  mas install 409203825
+  mas install 1278508951
+fi
 
 # ohmyzsh
+echo
 echo "${GREEN}Installing ohmyzsh!"
 echo
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
 clear
-echo "${GREEN}DONE!"
+echo "${GREEN}______ _____ _   _  _____ "
+echo "${GREEN}|  _  \  _  | \ | ||  ___|"
+echo "${GREEN}| | | | | | |  \| || |__  "
+echo "${GREEN}| | | | | | | .   ||  __| "
+echo "${GREEN}| |/ /\ \_/ / |\  || |___ "
+echo "${GREEN}|___/  \___/\_| \_/\____/ "
+
 echo
 echo
 printf "${RED}"
 read -s -k $'?Press ANY KEY to REBOOT\n'
 sudo reboot
 exit
-
-
-
-
-
-
-
-
-
